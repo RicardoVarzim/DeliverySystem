6ba8d96751befdf38a8f737a41fe25f9fdb309ba
Ricardo Varzim <ricardovarzim@gmail.com>
2019-02-05 19:42:16 +0000

    Tests with xUnit; FluentAssertions; Moq;

---
+++ tests/DeliveryService.Api/Controllers/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using DeliveryService.Api.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DeliveryService.Api.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void home_controller_get_should_return_string_content()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Get();
+
+            var contentResult = result as ContentResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Content.Should().BeEquivalentTo("Hello from DeliveryServiceApi");
+        }
+    }
+}
---
+++ tests/DeliveryService.Api/Controllers/PointsControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using DeliveryService.Api.Controllers;
+using DeliveryService.Common.Commands;
+using DeliveryService.Services.Points.Domain.Repositories;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using Xunit;
+
+namespace DeliveryService.Api.Tests.Controllers
+{
+    public class PointsControllerTests
+    {
+        [Fact]
+        public async void points_controller_post_should_return_accepted()
+        {
+            var busClientMock = new Mock<IBusClient>();
+            var pointsRepositoryMock = new Mock<IPointRepository>();           
+            var controller = new PointsController(busClientMock.Object);
+            var userId = Guid.NewGuid();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(
+                        new Claim[]
+                        {
+                            new Claim(ClaimTypes.Name, userId.ToString())
+                        }, "test"))
+                }
+            };
+
+            var command = new CreatePoint
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId
+            };
+
+            var result = await controller.Post(command);
+
+            var contentResult = result as AcceptedResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Location.Should().BeEquivalentTo("points/" + command.Id);
+
+        }
+
+    }
+}

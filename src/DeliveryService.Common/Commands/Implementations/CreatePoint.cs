e36c042cfcf167475581199019f2a94a3c4143b5
Ricardo Varzim <ricardovarzim@gmail.com>
2019-02-07 18:23:25 +0000

    fix missing appSettings on Api

--- src/DeliveryService.Common/Commands/Implementations/CreatePoint.cs
+++ src/DeliveryService.Common/Commands/Implementations/CreatePoint.cs
@@ -6,8 +6,8 @@ namespace DeliveryService.Common.Commands
 {
     public class CreatePoint : ICommand
     {
-        public Guid UserId { get; set; }
         public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
     }
